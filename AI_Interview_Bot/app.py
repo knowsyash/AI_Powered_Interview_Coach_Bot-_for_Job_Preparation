a93b0556388e46a36ef5b8c95b5110f2ecf0f7d5
knowsyash <codeknowsyash@gmail.com>
2026-05-08 17:17:04 +0530

    added

--- AI_Interview_Bot/app.py
+++ AI_Interview_Bot/app.py
@@ -373,5 +373,5 @@ def health_check():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    # Hugging Face Spaces expects the app to run on port 7860
+    app.run(host='0.0.0.0', port=7860, debug=False)
